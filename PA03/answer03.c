513f0ba5fbbb36a778781150abd41efd5a905fc1
Parth Pravin Patel <patel344@ecegrid-thin1.ecn.purdue.edu>
2014-09-15 12:52:16 -0400

    First three working (seg falut w/4)

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -1,5 +1,6 @@
 #include "answer03.h"
 #include <string.h>
+#include <stdio.h>
 
 char * strcat_ex(char * * dest, int * n, const char * src)
 {
@@ -10,10 +11,10 @@ char * strcat_ex(char * * dest, int * n, const char * src)
 	if ((*dest == NULL )|| (*n < (strlen(*dest) + strlen(src) + 1)))
 	{  
 		*n = 1 + 2 * (strlen(*dest) + strlen(src));
-		buffer =  (char*) malloc(*n);
+		buffer = (char*) malloc(*n * sizeof(char));
 		//*buffer = '\0';
-		//if(*dest != NULL){
-		buffer = strcpy(buffer,*dest);
+		//if(*dest != NULL)
+	    buffer = strcpy(buffer,*dest);
 		free(*dest);
 		*dest = buffer;
 		*dest = strcat(*dest,src);
@@ -31,44 +32,87 @@ char * * explode(const char * str, const char * delims, int * arrLen)
 	//Local Declarations
 	int N = 0;
 	int i = 0;
+	int k = 0;
 	int arrInd = 0; // this is the next position where we'll create a string
 	int last = 0; // 1 + the last index we saw a delimiter. Init to 0.
+	char * * strArr;
+	char * newArray;
 
-	//Executable Statements
+	//Executables Statements
 	// Count how many delimiters are in 'str'
-	for (i = 0; i < strlen(str); i++) 
+	for (i = 0; i < (strlen(str)); i++) 
 	{
   		if (strchr(delims, str[i]) != NULL)
   		{
   			N++;
   		}
 	}
-
 	// Create the return array
-	char * * strArr = malloc((N+1) * sizeof(char *));
+	strArr = malloc((N+1) * sizeof(char *));
 
 	//Filling the Arrays
 	for(i = 0; i < strlen(str); i++) 
     {
-    	if (strchr(delims, str[i]) != NULL)
+       
+    	if (strchr(delims,str[i]) != NULL)
     	 {
-    	 	*strArr = strArr[arrInd];
-    	 	last = i + 1;
-    	 	arrInd++;
-     	 }
+            newArray = malloc(strlen(str) * sizeof(char));
+    	 	for (k = last; k < i; k++)
+    	 	{
+                newArray[k-last] = str[k];
+    	 	}
+
+    	 newArray[k-last] = '\0';
+    	 strArr[arrInd] = newArray;
+    	 last = i + 1;
+    	 arrInd++;		
+     	 } 
     }
 
+    newArray = malloc(strlen(str) * sizeof(char));
     for(i = last; i < strlen(str); i++)
     {
-    	*strArr = strArr[N];
+    	newArray[i-last] = str[i];
     }
-    
-    *arrLen = strlen(*strArr);
-
+    newArray[i-last] = '\0';
+    strArr[N] = newArray;
+    *arrLen = N+1;
     return strArr;
-
 }
-char * implode(char * * strArr, int len, const char * glue){return 0;}
-void sortStringArray(char * * arrString, int len){}
+char * implode(char * * strArr, int len, const char * glue)
+{
+    int i;
+    int length;
+    char * newArray;
+    for(i = 0; i < len; i++)
+    {
+        length = strlen(strArr[i]);
+        length += length;
+    }
+
+    newArray = malloc((1 + 2 * length) * sizeof(char));
+    for(i = 0; i < len; i++)
+    {
+        strcat_ex(&newArray,&length,strArr[i]);
+        strcat_ex(&newArray,&length,glue);
+    }
+	return newArray;
+}
+ static int cmpstringp(const void *p1, const void *p2)
+ {
+    return strcmp(* (char * *) p1, * (char * *) p2);
+ }
+
+void sortStringArray(char * * arrString, int len)
+{
+    int i;
+
+    qsort(*arrString,len,sizeof(char*), cmpstringp);
+
+    for (i = 0; i < len; i++)
+    {
+        puts(arrString[i]);
+    }
+}
 void sortStringCharacters(char * str){}
 void destroyStringArray(char * * strArr, int len){} 
