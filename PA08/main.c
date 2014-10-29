0b12046b760c9d9ffe587a9a247fdcbad42af777
Parth Pravin Patel <patel344@ecegrid-thin5.ecn.purdue.edu>
2014-10-29 17:26:47 -0400

    main with robust test cases

--- PA08/main.c
+++ PA08/main.c
@@ -6,6 +6,7 @@ int comparstr(const char *p1, const char *p2);
 /*int comparstr(const char *p1, const char *p2) {
 		return strcmp(p1,p2);
 }*/
+List* append(List* result,List* list);
 void listprint(List* node);
 void listprint(List* node){
 	while(node != NULL){
@@ -34,25 +35,29 @@ int main(int argc, char** argv){
 	List* Sort2 = NULL;
 	List* Result = NULL;
 	
-	Sort1 = List_createNode("A");
-	Sort1->next = List_createNode("G");
-	Sort1->next->next = List_createNode("X");
+	Sort1 = NULL;
+	Sort2 = NULL;
+	//Sort1 = List_createNode("B");
+	//Sort1->next = List_createNode("G");
+	//Sort1->next->next = List_createNode("X");
 
-	Sort2 = List_createNode("Z");
-	Sort2->next = List_createNode("D");
-	Sort2->next->next = List_createNode("F");
-	Sort2->next->next->next = List_createNode("A");
+	//Sort2 = List_createNode("A");
+	//Sort2->next = List_createNode("D");
+	//Sort2->next->next = List_createNode("F");
+	//Sort2->next->next->next = List_createNode("Z");
 
 	// print the lists before sorting
 	printf("Sort1: "); listprint(Sort1);
 	printf("Sort2: "); listprint(Sort2);
 
-	//test_split(Sort1);
-	//test_split(Sort2);
+//	test_split(Sort1);
+//	test_split(Sort2);
 
-	//Result = List_merge(Sort2, comparstr);
-	//printf("\n\nMerged: "); listprint(Result);
+	Result = List_merge(Sort1,Sort2,comparstr);
+	printf("\n\nMerged: "); listprint(Result);
         
+//	Result = append(Sort1,Sort2);
+//	printf("\n\nappended: "); listprint(Result);
 	Result = List_sort(Sort2,comparstr);
 	printf("\n\nSorted: "); listprint(Result);	
 	return 0;
